61779bac1c37944e9a8023336e2fdf7d27efbd30
Ivan Gagarinov <dzencot@gmail.com>
2023-08-01 16:09:30 +0500

    add linter script

---
+++ linter_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+EXERCISE_KIT_PATH="/home/ivan/projects/hexlet/hexlet-exercise-kit"
+LANGUAGE=$1
+
+CURPATH=$(pwd)
+COURSES=$(find "${EXERCISE_KIT_PATH}/exercises/ru" -maxdepth 1 -regex ".*-${LANGUAGE}-.*")
+
+TEST_RESULT='error'
+
+echo '' > ${CURPATH}/result.txt
+
+for COURSE in $COURSES; do
+  REPO_PATHS=$(ls $COURSE)
+  # echo $REPO_PATHS
+  for REPO in $REPO_PATHS; do
+    FULL_PATH="${COURSE}/${REPO}"
+    echo ${FULL_PATH}
+    cd ${FULL_PATH}
+    make build
+    make start
+    TEST_OUTPUT=$(make lint-hexlet-js)
+    if [[ ${TEST_OUTPUT} == *${TEST_RESULT}* ]]; then
+      echo NOT OK
+      echo ${FULL_PATH} >> ${CURPATH}/result.txt
+    fi
+  done
+done
